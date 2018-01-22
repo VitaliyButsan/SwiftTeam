a07312cd7d5172daf6e0aa60f530ad7ae9e9611d
vitket <skolkojvastut@gmail.com>
2018-01-22 23:00:11 +0200

    Update

--- SwiftCode.playground/Contents.swift
+++ SwiftCode.playground/Contents.swift
@@ -1,25 +1,25 @@
 // The Declaration of arrays.
 var numbersArray = [2, 3, -3, 15, 25, 0, 32, 0, -8, 17]
 var positiveArray: [Int] = []
-
+let del = 0
 
 
 for numbers in numbersArray {
     
     switch numbers {
-    // Removing of zeros.
-    case 0:
-        if let ind = numbersArray.index(of: 0){ //Хорошо но не универсально, подумайте что написать вместо 0 здесь - .index(of: 0) чтобы было то что надо.
-            numbersArray.remove(at: ind)
-        }
-    // The output negatives numbers in the console.
-    case ...(-1):
-        print(numbers)
-    // Entering positive numbers into the array.
-    case 1... :
-        positiveArray.append(numbers)
+            // Removing of zeros.
+        case del:
+            if let ind = numbersArray.index(of: del){
+                numbersArray.remove(at: ind)
+            }
+            // The output negatives numbers in the console.
+        case ...(-1):
+            print(numbers)
+            // Entering positive numbers into the array.
+        case 1...:
+            positiveArray.append(numbers)
         
-    default: print("good bye my love")
+        default: print("good bye my love")
     }
     
 }
